28385ec02e026d5e41db31bfaa7c2c64c25a3b26
Rej Averion <averionrej@gmail.com>
2023-02-06 00:16:15 +0800

    added comments

--- 100doors.js
+++ 100doors.js
@@ -3,16 +3,19 @@ function getFinalOpenedDoors(numDoors) {
     let doors = [];
     let openDoors = [];
     
+    // build 100 doors all closed (false)
     for (let idx = 0; idx < numDoors; idx++) {
         doors[idx] = false;
     }
 
+    // toggle (true->false / false->true) every inc-th door
     for (let inc = 1; inc <= numDoors; inc++){
         for (let idx = inc - 1; idx < numDoors; idx = idx + inc) {
             doors[idx] = !doors[idx];
         }
     }
 
+    // return an array of all open (true) doors
     let openDoorsIdx = 0;
     for (let idx = 0; idx < numDoors; idx++) {
         if (doors[idx]) {
