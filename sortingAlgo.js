4947faf7f10ea9e57233566d08365efb9fc0800c
Rej Averion <averionrej@gmail.com>
2023-02-05 09:35:44 +0800

    update and rename bubblesort.js

--- sortingAlgo.js
+++ sortingAlgo.js
@@ -16,6 +16,10 @@ function bubbleSort(array) {
     // Only change code above this line
   }
 
+  function selectionSort(array) {
+    return array;
+  }
+
   var sortedArray = bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]);
 
   console.log(sortedArray);
