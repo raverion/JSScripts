bcfd193a79f2b3fdcb6c5b51c79bd379bbbdf3c3
Rej Averion <averionrej@gmail.com>
2023-02-05 12:00:39 +0800

    passing selectionSort()

--- sortingAlgo.js
+++ sortingAlgo.js
@@ -17,9 +17,42 @@ function bubbleSort(array) {
   }
 
   function selectionSort(array) {
-    return array;
+
+    let startIdx = 0;
+    let min;
+    let minIdx;
+    let newArray = [];
+    while (startIdx<array.length) {
+
+        // find the smallest element
+        for (let idx=0; idx<array.length; idx++) {
+            if (array[idx] != undefined) {
+                min = array[idx];
+                minIdx = idx;
+                break;
+            }
+        }
+        for (let idx=0; idx<array.length; idx++) {
+            if (array[idx] == undefined) {
+                continue;
+            }
+            if (array[idx]<min){
+                min = array[idx];
+                minIdx = idx;
+            }
+        }
+        array[minIdx] = undefined;
+    
+        // append to newArray
+        newArray[startIdx] = min;
+        startIdx++;
+    
+    }
+    return newArray;
   }
 
-  var sortedArray = bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]);
+  var bubbleArray = bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]);
+  var selectionArray = selectionSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]);
 
-  console.log(sortedArray);
+  console.log(bubbleArray);
+  console.log(selectionArray);
